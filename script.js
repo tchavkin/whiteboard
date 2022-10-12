fc30af94c001fd33f59f98bab3a2af72e95f54bd
Troy Chavkin <tachavkin@gmail.com>
2022-10-11 18:34:09 -0600

    whiteboard - click button to move div

---
+++ script.js
@@ -0,0 +1,9 @@
+function buttonClick() {
+    console.log(Math.random())
+}
+
+
+var d = document.getElementById('box2');
+d.style.position = "absolute";
+d.style.left = x_pos+'200px';
+d.style.top = y_pos+'200px';
