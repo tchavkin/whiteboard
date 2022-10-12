ebc8e9a6311fe284a32cdeca3a7a978b90a2d8fe
Troy Chavkin <tachavkin@gmail.com>
2022-10-11 19:08:48 -0600

    whiteboard-programming butoon tomove div

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+var d = document.getElementById('box2') 
+
 function buttonClick() {
-    console.log(Math.random())
+    d.style.color='blue';
+    d.style.position = "absolute";
+    d.style.left = '200px';
+    d.style.top = '200px';
 }
 
 
-var d = document.getElementById('box2');
-d.style.position = "absolute";
-d.style.left = x_pos+'200px';
-d.style.top = y_pos+'200px';
+
+
+
+
+console.log(Math.random())
+
+// 
+// 
+// d.style.left = x_pos+'200px';
+// d.style.top = y_pos+'200px';
+
